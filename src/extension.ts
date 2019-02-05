0394652935909d97ffcfd821c24879d19fbdfde4
Patrick <p.lauhof@gmail.com>
2019-02-06 00:34:42 +0100

    init

---
+++ src/extension.ts
@@ -0,0 +1,21 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(vscode.languages.registerCompletionItemProvider('*', {
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
+			let completionWords= ["Scenario","Scenarios","Feature","Given","When","Then"];
+			var items:vscode.CompletionItem[] = [];
+			completionWords.forEach(element => {
+				items.push(new vscode.CompletionItem(element+": "));
+			});
+			return items;
+		}
+		}));
+	}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
